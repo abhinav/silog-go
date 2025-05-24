757d52e53ad8b3654e11e1472eed5b425581234f
Abhinav Gupta <mail@abhinavg.net>
2025-05-24 12:18:36 -0700

    internal/log: rename to silog (abhinav/git-spice#677)
    The name "log" conflicts with the variable name "log"
    which is used frequently in the codebase,
    making it difficult to access log.NonZero and possible extensions.

---
+++ level_test.go
@@ -0,0 +1,62 @@
+package silog_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.abhg.dev/log/silog"
+)
+
+func TestLevel_String(t *testing.T) {
+	tests := []struct {
+		level    silog.Level
+		expected string
+	}{
+		{silog.LevelDebug, "debug"},
+		{silog.LevelInfo, "info"},
+		{silog.LevelWarn, "warn"},
+		{silog.LevelError, "error"},
+		{silog.LevelFatal, "fatal"},
+		{silog.Level(100), "ERROR+92"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.expected, func(t *testing.T) {
+			assert.Equal(t, tt.expected, tt.level.String())
+		})
+	}
+}
+
+func TestByLevel_Get(t *testing.T) {
+	byLevel := silog.ByLevel[string]{
+		Debug: "debug",
+		Info:  "info",
+		Warn:  "warn",
+		Error: "error",
+		Fatal: "fatal",
+	}
+
+	tests := []struct {
+		level silog.Level
+		want  string
+	}{
+		{silog.LevelDebug, "debug"},
+		{silog.LevelInfo, "info"},
+		{silog.LevelWarn, "warn"},
+		{silog.LevelError, "error"},
+		{silog.LevelFatal, "fatal"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.want, func(t *testing.T) {
+			got := byLevel.Get(tt.level)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+
+	t.Run("unknown", func(t *testing.T) {
+		assert.Panics(t, func() {
+			byLevel.Get(silog.Level(100))
+		})
+	})
+}
---
+++ writer_test.go
@@ -0,0 +1,31 @@
+package silog_test
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.abhg.dev/log/silog"
+)
+
+func TestLogWriter(t *testing.T) {
+	var buf bytes.Buffer
+	logger := silog.New(&buf, nil)
+	writer, done := silog.Writer(logger, silog.LevelInfo)
+
+	_, err := fmt.Fprint(writer, "hello world")
+	require.NoError(t, err)
+	done()
+
+	assert.Equal(t, "INF hello world\n", buf.String())
+}
+
+func TestLogWriter_nil(t *testing.T) {
+	writer, done := silog.Writer(nil, silog.LevelInfo)
+
+	_, err := fmt.Fprint(writer, "hello world")
+	require.NoError(t, err)
+	done()
+}
