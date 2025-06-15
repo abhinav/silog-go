ae157524490d061c469de98b6d9aee0893bd0d6a
Abhinav Gupta <mail@abhinavg.net>
2025-06-15 11:03:40 -0700

    More exmaples

--- example_test.go
+++ example_test.go
@@ -35,7 +35,7 @@ func Example_customLevel() {
 }
 
 // Demonstrates reserving a log level to be logged without a label before it.
-func Example_noLogLabel() {
+func Example_noLabel() {
 	const LevelPlain = slog.LevelDebug - 1
 
 	style := silog.PlainStyle()
@@ -57,6 +57,42 @@ func Example_noLogLabel() {
 	// Output:
 	// This is a plain message
 	// DBG This is a debug message
+}
+
+func ExampleHandler_WithPrefix() {
+	handler := silog.NewHandler(os.Stdout, &silog.HandlerOptions{
+		Style: silog.PlainStyle(),
+		// To keep the test output clean easy to test,
+		// we will not log the time in this example.
+		ReplaceAttr: skipTime,
+	})
+
+	logger := slog.New(handler.WithPrefix("example"))
+	logger.Info("This is an info message")
+
+	// Output:
+	// INF example: This is an info message
+}
+
+func ExampleHandler_WithLevelOffset() {
+	handler := silog.NewHandler(os.Stdout, &silog.HandlerOptions{
+		Level: slog.LevelDebug,
+		Style: silog.PlainStyle(),
+		// To keep the test output clean easy to test,
+		// we will not log the time in this example.
+		ReplaceAttr: skipTime,
+	})
+
+	logger := slog.New(handler.WithLevelOffset(-4)) // downgrade messages
+	logger.Error("Downgraded to warning")
+	logger.Warn("Downgraded to info")
+	logger.Info("Downgraded to debug")
+	logger.Debug("Not logged because below configured level")
+
+	// Output:
+	// WRN Downgraded to warning
+	// INF Downgraded to info
+	// DBG Downgraded to debug
 }
 
 func skipTime(groups []string, attr slog.Attr) slog.Attr {
